d3d4e5a3bdbd0e0a48c6e17704526f191443f5c9
Yoan Garcia Perez <Yoan.GarciaPerez@nov.com>
2021-11-18 12:56:33 -0600

    solution

---
+++ Counting-Valleys/Counting-Valleys/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counting_Valleys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            countingValleys(8, "UDDDUDUU");
+            //countingValleys(8, "DDUUDDUDUUUD");
+        }
+
+        public static int countingValleys(int steps, string path)
+        {
+            int level = 0;
+            bool valley = false;
+            int numberOfValleys = 0;
+            foreach (var item in path)
+            {
+                if (item == 'U')
+                    level++;
+                else if (item == 'D')
+                    level--;
+                if (level < 0 && !valley)
+                {
+                    numberOfValleys++;
+                    valley = true;
+                }                    
+                else if(level == 0 && valley)
+                {
+                    valley = false;                    
+                }
+
+            }
+            return numberOfValleys;
+        }
+    }
+}
